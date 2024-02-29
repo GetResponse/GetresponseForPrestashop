229a22ac825aba699e9a260bc6a4ba2d08d2a283
Bartłomiej Stępień <bstepien@getresponse.com>
2024-02-29 13:35:13 +0100

    GRINTEGRAT-5323 - added standard fixes

--- tests/phpstan/index.php
+++ tests/phpstan/index.php
@@ -17,31 +17,14 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
-if (!defined('_PS_VERSION_')) {
-    exit;
-}
 
-class WebserviceRequest extends WebserviceRequestCore
-{
-    public function __construct()
-    {
-        include_once _PS_MODULE_DIR_ . 'grprestashop/classes/WebserviceSpecificManagementGetresponseModule.php';
-    }
 
-    public static function getResources()
-    {
-        $resources = WebserviceRequestCore::getResources();
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
 
-        if (version_compare(_PS_VERSION_, '1.7', '>=')) {
-            return $resources;
-        }
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
-        $resources['getresponse_module'] = [
-            'description' => 'Getresponse Integration',
-            'specific_management' => true,
-        ];
-        ksort($resources);
-
-        return $resources;
-    }
-}
+header("Location: ../");
+exit;
