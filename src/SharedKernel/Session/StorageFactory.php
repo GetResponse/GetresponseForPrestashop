20b0399278afc60b8c77b49106a7569a4e8835b0
Daniel Kłoda <daniel.kloda@getresponse.com>
2024-10-31 12:03:20 +0100

    GRINTEGRAT-5613 - add error logging

--- src/SharedKernel/Session/StorageFactory.php
+++ src/SharedKernel/Session/StorageFactory.php
@@ -9,19 +9,29 @@ class StorageFactory
 {
     public function create()
     {
-        $context = \Context::getContext();
+        try {
+            $context = \Context::getContext();
 
-        /** @var SessionInterface|null $session */
-        $session = $context->session;
+            /** @var SessionInterface|null $session */
+            $session = $context->session;
 
-        if ($session instanceof SessionInterface) {
-            return new SessionStorage($session);
+            if ($session instanceof SessionInterface) {
+                return new SessionStorage($session);
+            }
+
+            if ($context->cookie) {
+                return new CookieStorage($context->cookie);
+            }
+        } catch (RuntimeException $e) {
+            $this->logGetResponseError($e->getMessage());
         }
+    }
 
-        if ($context->cookie) {
-            return new CookieStorage($context->cookie);
-        }
-
-        throw new RuntimeException('No suitable storage available in the current context.');
+    /**
+     * @param string $message
+     */
+    private function logGetResponseError($message)
+    {
+        PrestaShopLoggerCore::addLog($message, 2, null, 'GetResponse', 'GetResponse');
     }
 }
